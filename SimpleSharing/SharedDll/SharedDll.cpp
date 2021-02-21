05ef05431f8a77967f727fc25aeb3edee22328be
Duane Michael <dmichaeljr@pm.me>
2021-02-21 16:05:15 -0700

    Update.

--- SimpleSharing/SharedDll/SharedDll.cpp
+++ SimpleSharing/SharedDll/SharedDll.cpp
@@ -1,20 +1,21 @@
-// SharedDll.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "stdafx.h"
 
-#include <iostream>
+#pragma comment(lib,"..\\x64\\debug\\SharedDll.lib")
+__declspec(dllimport) int Counter;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+int _tmain(int argc, _TCHAR* argv[]) {
+
+	for (;;) {
+
+		std::cout << "Count = " << Counter << std::endl;
+		std::cout << "Enter number to add, -1 to quit: ";
+		int number;
+		std::cin >> number;
+		if (number < 0)
+			break;
+		Counter += number;
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
