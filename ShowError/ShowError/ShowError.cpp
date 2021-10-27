2b02b3b90bfaad02b89a8b4453b47965630436c9
Duane Michael <dmichaeljr@pm.me>
2021-10-26 23:21:17 -0600

    ShowError

---
+++ ShowError/ShowError/ShowError.cpp
@@ -0,0 +1,35 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main(int argc, const char* argv[]) {
+
+    if (argc < 2) {
+        printf("Usage: ShowError <number>\n");
+        return 0;
+    }
+
+    int message = atoi(argv[1]);
+
+    LPWSTR text;
+    DWORD chars = ::FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |        // Function allocates
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        nullptr,
+        message,
+        0,
+        (LPWSTR)&text,
+        0,
+        nullptr
+    );
+
+    if (chars > 0) {
+        printf("Message %d: %ws\n", message, text);
+        ::LocalFree(text);
+    }
+    else {
+        printf("No such error exists\n");
+    }
+
+    return 0;    
+}
